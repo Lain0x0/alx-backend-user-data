ef80bec2ac56bbda8a8fd1c89c3d9180fd5fb42f
Lain0x0 <lainyamato03@gmail.com>
2024-09-12 18:19:33 +0000

    0x2-Pll

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -20,6 +20,19 @@ def filter_datum(
     return re.sub(extract(fields, separator), replace(redaction), message)
 
 
+def get_logger() -> logging.Logger:
+    """Configuring & returning to handle
+        user data logs
+        """
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
+
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter
         class fields in log msg
