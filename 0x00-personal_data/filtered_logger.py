79fabd4529ea46f94aee226ce303d22f89cb4514
Lain0x0 <lainyamato03@gmail.com>
2024-09-12 18:08:46 +0000

    0x1-Pll

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,3 +18,26 @@ def filter_datum(
     """Intialize filter_datum propriety"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter
+        class fields in log msg
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        mesg = super(RedactingFormatter, self).format(record)
+        text = filter_datum(self.fields, self.REDACTION, mesg, self.SEPARATOR)
+        return text
+
+
+if __name__ == "__main__":
+    main()
