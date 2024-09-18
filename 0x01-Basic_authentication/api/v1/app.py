3bdb450d51e8282f587ddb3b3fb12704094ebbd5
Lain0x0 <lainyamato03@gmail.com>
2024-09-18 21:24:44 +0000

    update fix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,12 @@ def unauthorized() -> None:
     abort(401)
 
 
+@app.errorhandler(401)
+def unauthorized_error(error) -> tuple:
+    """Return a JSON response for 401 Unauthorized error."""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
