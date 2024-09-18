759326a6c73387649d2c941bf9199d3fa7726014
Lain0x0 <lainyamato03@gmail.com>
2024-09-18 21:48:03 +0000

    0x0-fix please

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,10 +6,33 @@ import os
 from flask import Flask, jsonify, abort
 from flask_cors import CORS, cross_origin
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+
+auth = None
+if os.getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+
+
+@app.before_request
+def before_request_func():
+    """bla bla bla annoted"""
+    if auth is None:
+        return
+    if not auth.require_auth(request.path, ['/api/v1/status/',
+                                            '/api/v1/unauthorized/',
+                                            '/api/v1/forbidden/']):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 @app_views.route('/unauthorized/', strict_slashes=False)
@@ -25,6 +48,12 @@ def unauthorized() -> None:
 def unauthorized_error(error) -> tuple:
     """Return a JSON response for 401 Unauthorized error."""
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Forbidden handler."""
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
