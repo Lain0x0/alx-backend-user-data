ae02521c68f64c1d6027be2a859d473cfb86c734
Lain0x0 <lainyamato03@gmail.com>
2024-09-19 21:11:14 +0000

    fix

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-
+from api.v1.auth.session_exp_auth import SessionExpAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,6 +22,12 @@ if os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
 elif os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+if auth_type == 'session_auth':
+    auth = SessionAuth()
+if auth_type == 'session_exp_auth':
+    auth = SessionExpAuth()
 
 
 @app_views.route('/unauthorized/', strict_slashes=False)
