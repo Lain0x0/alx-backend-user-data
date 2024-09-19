e999d24dd19f35809602b257fbc0da7ba9654d28
Lain0x0 <lainyamato03@gmail.com>
2024-09-19 19:34:00 +0000

    fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""User Basic 
+"""User Basic
 authentication
 Session
 """
@@ -13,3 +13,10 @@ from models.user import User
 class SessionAuth(Auth):
     """Session authentication class"""
     user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user"""
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
