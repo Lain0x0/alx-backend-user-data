7bc564103c6a33144bef00ddfab41bc5ebcf47a6
Lain0x0 <lainyamato03@gmail.com>
2024-09-19 19:54:22 +0000

    fix 2

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -37,3 +37,9 @@ class Auth:
         """Method get current user
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Gets the value of the cookie named SESSION_NAME"""
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
