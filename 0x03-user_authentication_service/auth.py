b26228f44f0052810e62220b8f271bf58ae8bd99
Lain0x0 <lainyamato03@gmail.com>
2024-09-24 23:50:52 +0000

    0x1-auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 """ Hashing Passwords Using bcrypt"""
 import bcrypt
+from db import DB
 
 
 def _hash_password(password: str) -> str:
     """bcrypt for pass hashing"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode()
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """User Registration"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
