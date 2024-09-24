febdfab6d3c4ef97da800a5fb00e2d1899da8b68
Lain0x0 <lainyamato03@gmail.com>
2024-09-24 20:40:07 +0000

    fix 0x0-db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """DB module
 """
+from user import Base, User
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
-from user import Base
 
 
 class DB:
