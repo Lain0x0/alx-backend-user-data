f7dca73c17655370011c90158e31cbe7b92ffe1e
Lain0x0 <lainyamato03@gmail.com>
2024-09-25 15:31:54 +0000

    0x4-auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,3 +46,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """Getting user from a session ID"""
+        if session_id is None:
+            return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
