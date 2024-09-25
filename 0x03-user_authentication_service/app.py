4e1f82d84a76f77df2adacb6aee6721120c15823
Lain0x0 <lainyamato03@gmail.com>
2024-09-25 15:49:22 +0000

    0x2-app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,5 +26,21 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """POST /sessions"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    valid = AUTH.valid_login(email, password)
+    if not valid:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
